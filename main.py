f4820a8f3951f028c295cdbd4088271d9fedec88
RahulSikdar-lab <sikdar313212@gmail.com>
2025-03-03 23:07:44 +0530

    game code and file to store score

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import random
+n = random.randint(1,100)
+a=-1
+guesses = 1
+while(a!=n):
+    a = int(input("Guess the number:"))
+    if(a>n):
+        print("Lower Number Please")
+        guesses = guesses+1
+    elif(a<n):
+        print("Higher Number Please")
+        guesses=guesses+1
 
+print(f"you have guessed the number {n} correctly in {guesses} attempts")
+def score(guesses):
+    try:
+       
+        with open("highest-score.txt", "r") as file:
+            highest_score = file.read()
+            if highest_score != "":
+                highest_score = int(highest_score)
+            else:
+                highest_score = float('inf')  
+    except FileNotFoundError:
+        
+        highest_score = float('inf')
+
+    print(f"Current highest score: {highest_score} guesses.")
+    
+    
+    if guesses < highest_score:
+        
+        with open("highest-score.txt", "w") as file:
+            file.write(str(guesses))
+        print(f"New highest score saved! {guesses} guesses.")
+    else:
+        print(f"The highest score remains {highest_score} guesses.")
+
+score(guesses)
+
+    
+    
